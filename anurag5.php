fbf3d0276ec8f7309ef4c1bbd4f5f2a2cfafe223
Anurag Mishra <155640865+AK-UP78@users.noreply.github.com>
2024-12-15 10:17:25 +0530

    Add files via upload

---
+++ anurag5.php
@@ -0,0 +1,7 @@
+<?php
+function generateRandomString($length = 10) {
+    return substr(str_shuffle('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'), 0, $length);
+}
+
+echo generateRandomString();
+?>
